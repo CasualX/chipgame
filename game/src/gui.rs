b9cde726f26c54202eb6583289025a47dbb9cd58
Casper <CasualX@users.noreply.github.com>
2024-11-07 00:42:44 +0100

    Levels and hud scratchpad

---
+++ game/src/gui.rs
@@ -0,0 +1,14 @@
+use cvmath::Vec2f;
+
+pub struct Panel {
+	pub pos: Vec2f,
+}
+
+// impl Panel {
+// 	pub fn panel(&self) -> Panel {
+
+// 	}
+
+// 	pub fn fill(&self, )
+// }
+
--- game/src/lib.rs
+++ game/src/lib.rs
@@ -7,6 +7,7 @@ pub mod fx;
 pub mod menu;
 pub mod play;
 pub mod save;
+pub mod gui;
 
 #[derive(serde::Serialize, serde::Deserialize)]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
